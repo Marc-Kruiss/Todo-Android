c84bef0ab28993561cf44f64a748f279af4f29a3
Marc Kruiß <k.marc@a1.net>
2022-01-07 19:52:49 +0100

    Setup Todo room-database/Dao

---
+++ app/src/main/java/com/example/todo_mvvm/data/TodoDao.kt
@@ -0,0 +1,16 @@
+package com.example.todo_mvvm.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTodo(todo:Todo)
+    @Delete
+    suspend fun deleteTodo(todo:Todo)
+    @Query("Select * from todo where id = :id")
+    suspend fun getTodoById(id:Int):Todo?
+    @Query("Select * from todo")
+    fun getTodos():Flow<List<Todo>>
+}
